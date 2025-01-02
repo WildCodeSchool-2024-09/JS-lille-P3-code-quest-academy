7c76818cd8f9658aa0e2a00062f1d2122ac34c56
Arnaud6216 <guevaer8@gmail.com>
2025-01-02 11:19:06 +0100

    adding a route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
-import Game from "./pages/game/Game";
 
 function App() {
   return (
     <>
-      <Game />
+      <Outlet/>
     </>
   );
 }
