46861f860e79b3b9a8454cc1975cf806c12e8348
Emeric Lesage <elesage.dev@gmail.com>
2025-01-15 14:18:42 +0100

    update db

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -2,7 +2,9 @@ CREATE TABLE account (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   username VARCHAR(50) NOT NULL UNIQUE,
   email VARCHAR(100) NOT NULL UNIQUE,
-  password VARCHAR(100) NOT NULL
+  password VARCHAR(100) NOT NULL,
+  teacher_1 VARCHAR(50) DEFAULT "Fantine",
+  teacher_2 VARCHAR(50) DEFAULT "Soufiane"
 );
 
 CREATE TABLE room (
@@ -17,8 +19,8 @@ CREATE TABLE challenge (
   title VARCHAR(50) NOT NULL,
   guideline TEXT NOT NULL,
   hint TEXT NOT NULL,
-  soluce TEXT NOT NULL,
-  type TEXT NOT NULL,
+  soluce VARCHAR(255) NOT NULL,
+  type VARCHAR(10) NOT NULL,
   question TEXT NOT NULL,
   rep1 VARCHAR(100),
   rep2 VARCHAR(100),
@@ -32,7 +34,7 @@ CREATE TABLE progress (
   user_id INT,
   room_id INT,
   challenge_id INT,
-  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES account(id),
+  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES account(id) ON DELETE CASCADE,
   CONSTRAINT fk_room FOREIGN KEY (room_id) REFERENCES room(id),
   CONSTRAINT fk_challenge FOREIGN KEY (challenge_id) REFERENCES challenge(id)
 );
@@ -57,24 +59,21 @@ VALUES
 
 INSERT INTO challenge (title, guideline, hint, soluce, type, question, rep1, rep2, rep3, rep4)
 VALUES
-  ("HTML", "Réponds à la question suivante en selectionnant la bonne réponse.", "astuce 1", "Blanc", "quizz", "Quelle est la couleur du cheval blanc d'Henri IV ?", "Noir", "Blanc", "C'est quoi un cheval ?", "La réponse D"),
-  ("HTML", "guideline 2", "astuce 2", "reponse 2", "prompt", "question 2", "null", "null", "null", "null"),
-  ("HTML", "guideline 3", "astuce 3", "reponse 3", "prompt", "question 3", "null", "null", "null", "null"),
-  ("CSS", "guideline 1", "astuce 1", "reponse 1", "quizz", "question 1", "A", "B", "C", "D"),
-  ("CSS", "guideline 2", "astuce 2", "reponse 2", "prompt", "question 2", "null", "null", "null", "null"),
-  ("CSS", "guideline 3", "astuce 3", "reponse 3", "prompt", "question 3", "null", "null", "null", "null"),
-  ("Javascript", "guideline 1", "astuce 1", "reponse 1", "quizz", "question 1", "A", "B", "C", "D"),
-  ("Javascript", "guideline 2", "astuce 2", "reponse 2", "prompt", "question 2", "null", "null", "null", "null"),
-  ("Javascript", "guideline 3", "astuce 3", "reponse 3", "prompt", "question 3", "null", "null", "null", "null"),
-  ("React", "guideline 1", "astuce 1", "reponse 1","quizz", "question 1", "A", "B", "C", "D"),
-  ("React", "guideline 2", "astuce 2", "reponse 2","prompt", "question 2", "null", "null", "null", "null"),
-  ("React", "guideline 3", "astuce 3", "reponse 3","prompt", "question 3", "null", "null", "null", "null"),
-  ("Node.js", "guideline 1", "astuce 1", "reponse 1","quizz", "question 1", "A", "B", "C", "D"),
-  ("Node.js", "guideline 2", "astuce 2", "reponse 2","prompt", "question 2", "null", "null", "null", "null"),
-  ("Node.js", "guideline 3", "astuce 3", "reponse 3","prompt", "question 3", "null", "null", "null", "null"),
-  ("SQL", "guideline 1", "astuce 1", "reponse 1","quizz", "question 1", "A", "B", "C", "D"),
-  ("SQL", "guideline 2", "astuce 2", "reponse 2","prompt", "question 2", "null", "null", "null", "null"),
-  ("SQL", "guideline 3", "astuce 3", "reponse 3","prompt", "question 3", "null", "null", "null", "null");
+  ("HTML", "guideline", "hint", "<img>", "quizz", "Quel est l’élément HTML utilisé pour insérer une image dans une page web ?", "<img>", "<image>", "<picture>", "<media>"),
+  ("HTML", "guideline", "hint", "<a>", "quizz", "Quelle est la balise correcte pour créer un lien hypertexte", "<link>", "<href>", "<a>", "<anchor>"),
+  ("HTML", "guideline", "hint", "id", "prompt", "Quel attribut est utilisé pour spécifier un identifiant unique à un élément HTML ?", "null", "null", "null", "null"),
+  ("HTML", "guideline", "hint", "Fournir une description alternative pour l’image si elle ne peut pas être affichée", "prompt",  "Quelle est la signification de l’attribut alt dans la balise <img> ?", "description", "alternative", "null", "null"),
+  ("CSS", "guideline", "hint", "color", "quizz", "Quelle propriété CSS est utilisée pour changer la couleur du texte ?", "background-color", "text-color", "font-color", "color"),
+  ("CSS", "guideline", "hint", "z-index", "quizz", "Quelle propriété CSS permet de contrôler l’ordre d’affichage des éléments empilés les uns sur les autres ?", "stack-order", "z-index", "order", "visibility"), 
+  ("CSS", "guideline", "hint", "var(--nom-de-la-variable)", "quizz", "Quelle syntaxe est correcte pour utiliser une variable CSS ?", "$nom-de-la-variable", "var(--nom-de-la-variable)", "variable(--nom-de-la-variable)", "%nom-de-la-variable"),
+  ("CSS", "guideline", "hint", "fixed", "prompt", "Quelle valeur de la propriété position permet de fixer un élément par rapport à la fenêtre, même lors du défilement de la page ?", "null", "null", "null", "null"),
+  ("Javascript", "guideline", "hint", "console.log('hello')", "quizz", "Quelle est la syntaxe correcte pour afficher un message dans la console ?", "print('hello')", "console.log('hello')", "echo('hello')", "console.log(hello)"),
+  ("Javascript", "guideline", "hint", "parseInt()", "quizz", "Quelle méthode est utilisée pour transformer une chaîne en nombre entier ?", "TransformToNumber()", "Number()", "String()", "parseInt()"),
+  ("Javascript", "guideline", "hint", "stocker donnée", "prompt", "Qu'est-ce qu'une variable en JavaScript ?", "null", "null", "null", "null"),
+  ("Javascript", "guideline", "hint", "const let var", "prompt", "Quels mots-clés peuvent être utilisés pour déclarer une variable ?", "null", "null", "null", "null"),
+  ("Javascript", "guideline", "hint", "document object model", "prompt", "Que signifie DOM ?", "null", "null", "null", "null"),
+  ("Javascript", "guideline", "hint", "undefined", "prompt", "Quel est le type par défaut d'une variable non initialisée ?", "null", "null", "null", "null");
+
 INSERT INTO progress (level, user_id, room_id, challenge_id)
 VALUES
   (1, 1, 1, 1),
