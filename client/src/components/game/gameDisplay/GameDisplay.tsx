183dbedb26079c7e8a640d5468f2d38ad1d4047b
Arnaud6216 <guevaer8@gmail.com>
2025-01-16 17:26:55 +0100

    remove room selection div

--- client/src/components/game/gameDisplay/GameDisplay.tsx
+++ client/src/components/game/gameDisplay/GameDisplay.tsx
@@ -20,16 +20,6 @@ function GameBoard() {
           src="./src/assets/images/game-background-level0.png"
           alt="plateau de jeu"
         />
-        {/* Éléments superposés pour selectionner la salle */}
-        <div className="overlay-item item1" />
-        <div className="overlay-item item2" />
-        <div className="overlay-item item3" />
-        <div className="overlay-item item4" />
-        <div className="overlay-item item5" />
-        <div className="overlay-item item6" />
-        <div className="overlay-item item7" />
-        <div className="overlay-item item8" />
-        <div className="overlay-item item9" />
       </div>
     </>
   );
