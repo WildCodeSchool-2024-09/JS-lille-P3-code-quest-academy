cf5097eddaeaa7f995f700d8c1e0c0e79d4f5652
Lesage Emeric <elesage.dev@gmail.com>
2025-01-30 11:18:13 +0100

    Delete server/src/types/types.ts

--- server/src/types/types.ts
+++
@@ -1,9 +0,0 @@
-export type Account = {
-  id: number;
-  username: string;
-  email: string;
-  hashed_password: string;
-  is_admin: boolean;
-  firstTeacher: string;
-  secondTeacher: string;
-};
